4c5d4fc6cff1e1556a85b987d483d44a561eadca
JinHyukKo <112324054+JinhyukKo@users.noreply.github.com>
2025-02-09 19:29:19 +0100

    Create handle_redirection.h

---
+++ header/handle_redirection.h
@@ -0,0 +1,8 @@
+
+
+#ifndef HANDLE_REDIRECTION_H
+#define HANDLE_REDIRECTION_H
+
+void handle_input_redirection(const char *input_file);
+void handle_output_redirection(const char *output_file);
+#endif //HANDLE_REDIRECTION_H
